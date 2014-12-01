69a530227a92cf3f02ae206535833faaf260cdf5
sparamonov <sergey.paramonov@cs.kuleuven.be>
2014-12-01 16:08:41 +0100

    updaded visualization to make boxplot

--- scripts/visualize_top.R
+++ scripts/visualize_top.R
@@ -3,8 +3,8 @@ library(ggplot2)
 source("scripts/auxiliary_visualization_functions.R")
 
 data <- read.csv("results/top/raw", header=TRUE, stringsAsFactors=FALSE)
-visual <- ggplot(data, aes(x=dummy_index, y=runtime, colour=dataset)) 
-visual <- visual + geom_smooth(se=FALSE, size=1.3) + geom_point() + theme_bw()
-visual <- visual + scale_x_continuous("pattern index") + scale_y_continuous("Runtime in seconds") + scale_color_manual(values=get_colors())
+visual <- ggplot(data, aes(x=dataset, y=runtime, colour=dataset)) 
+visual <- visual + geom_boxplot(se=FALSE, size=1.3) +  theme_bw()
+visual <- visual + scale_y_continuous("Runtime in seconds", limits=c(0,50000)) + scale_x_discrete("") # two points outside 
 ggsave(visual,file="figure_top_one_experiment.pdf")
 print(visual)
